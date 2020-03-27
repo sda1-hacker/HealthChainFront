fe7de0a41561b47808eaa2851de04a8714f73ef2
blg <760557109@qq.com>
2020-03-27 16:27:21 +0800

    init branch

--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -66,7 +66,7 @@ export default {
           // 设置token，记录登录状态
           window.sessionStorage.setItem('token', res._data.token.toString())
           that.$router.push("/admin")     // 调转到admin路由
-        } else{w
+        } else{
           layui.use(['layer'], function(){layui.layer.msg('用户名或密码不正确')})
         }
       })
