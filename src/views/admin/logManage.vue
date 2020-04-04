04ccf3c70e1a8a0cc7080e2609a8b2953f1e09a5
bilongge <760557109@qq.com>
2020-04-04 14:24:36 +0800

    整合node

--- src/views/admin/logManage.vue
+++ src/views/admin/logManage.vue
@@ -1,11 +1,11 @@
 <template>
 <div>
 <table class="layui-hide" id="demo" lay-filter="test"></table>
-<script type="text/html" id="operate-barDemo">
+<!-- <script type="text/html" id="operate-barDemo">
   <a class="layui-btn layui-btn-primary layui-btn-xs" lay-event="detail">查看</a>
   <a class="layui-btn layui-btn-xs" lay-event="edit">编辑</a>
   <a class="layui-btn layui-btn-danger layui-btn-xs" lay-event="del">删除</a>
-</script>
+</script> -->
 </div>
 </template>
 <script>
@@ -22,17 +22,19 @@ export default {
       table.render({
         elem: '#demo'
         ,height: 420
-        ,url: '/layuiadmin/json/table/user.js' //数据接口
+        ,url: http + '/api/admin/getLogList' //数据接口
         ,title: '用户表'
+        ,method:'post'
+        ,where:{token:window.sessionStorage.getItem('token')}
         ,page: true //开启分页
 
         ,cols: [[ //表头
           ,{field: 'id', title: 'ID', width:80, sort: true, fixed: 'left'}
-          ,{field: 'username', title: '操纵人', width:80}
-          ,{field: 'experience', title: '操作详情', width: 90, sort: true, totalRow: true}
-          ,{field: 'sex', title: '操作结果', width:80, sort: true}
-          ,{field: 'score', title: '操作时间', width: 80, sort: true, totalRow: true}
-          ,{fixed: 'right', align:'center', title:'操作',toolbar: '#operate-barDemo', width:'17%'}
+          ,{field: 'operateId', title: '操作人编号', width:80}
+          ,{field: 'operateDetails', title: '操作详情', width: 90, sort: true, totalRow: true}
+          ,{field: 'operateResult', title: '操作结果', width:80, sort: true}
+          ,{field: 'operateTime', title: '操作时间', width: 80, sort: true, totalRow: true}
+          // ,{fixed: 'right', align:'center', title:'操作',toolbar: '#operate-barDemo', width:'17%'}
         ]]
       });
 
